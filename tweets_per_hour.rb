cf68f6422f5bb117306ce5a48f094efd907be93e
Steve Dierker <dierker.steve@fu-berlin.de>
2010-12-29 22:38:54 +0100

    added script to calculate tweets per hour

---
+++ tweets_per_hour.rb
@@ -0,0 +1,31 @@
+#This script was written by Steve Dierker on 27C3 and
+#stands under the GPL v3.0.
+#http://www.gnu.org/licenses/gpl.html
+
+require 'twitter'
+
+search = Twitter::Search.new
+HASHTAG = "27c3"
+startid = 20192516867358720
+
+if File.exists?("id.store")
+  id_store = File.new("id.store","r")
+  startid = id_store.readline.to_i
+end
+starttime = Twitter::Search.new.hashtag(HASHTAG).max(startid).fetch.first.created_at
+
+current_page = search.hashtag(HASHTAG).since_id(startid).per_page(100).fetch
+next_id = current_page.first.id
+
+counter = current_page.count
+
+while search.next_page?() do
+  current_page = search.fetch_next_page
+  counter += current_page.count
+end
+quotient = (counter / (Time.now - Time.parse(starttime)))*60*60
+index = File.new("html/index.html", "w")
+index.write "#{quotient.truncate}\n"
+
+id_store = File.new("id.store","w")
+id_store.write "#{next_id}"
